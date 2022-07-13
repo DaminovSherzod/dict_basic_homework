79f82604d4eb0b7c4c3b50c23c3b6a8ba795b13d
Sherzod <daminovsherzod2002@gmail.com>
2022-07-13 14:08:47 +0500

    cities_dict.py

--- cities_dict.py
+++ cities_dict.py
@@ -6,4 +6,9 @@ def cities_dict(cities:list):
     Returns:
         dict: dictionary with keys ordered by city name
     """
-    return
+    x = {}
+    for i in range(len(cities)):
+        x[i] = cities[i]
+    return x
+
+print(cities_dict(['Bukhara','Khiva','Namangan','Samarkand','Tashkent']))    
